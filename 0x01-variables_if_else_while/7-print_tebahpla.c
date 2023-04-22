69f46dbe9e6fe10ff58c0197074c2e714ee7464b
ikrambl <ikrambelcadi@gmail.com>
2023-04-22 14:23:30 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - C program that prints lc letters
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int a = 122;
+
+	for (a = 122; a >= 97; a--)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
