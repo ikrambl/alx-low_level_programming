e3dea92e57947da314e25413610563223e5d186d
ikrambl <ikrambelcadi@gmail.com>
2023-04-22 13:44:58 -0700

    print digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - a C program prints single digit numbers
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int a = 0;
+
+	for (a = 0 ; a <= 9 ; a++)
+	{
+		printf("%d", a);
+	}
+	printf("\n");
+	return (0);
+}
