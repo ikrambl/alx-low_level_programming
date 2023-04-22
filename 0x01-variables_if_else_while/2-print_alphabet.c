72e4882807bd663da0678b80aa3f24189d99bc3a
ikrambl <ikrambelcadi@gmail.com>
2023-04-22 12:46:52 -0700

    a program that prints the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - a c program printing upper-c letters
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i = 97;
+
+	for (i = 97 ; i <= 122 ; i++)
+	{
+		putchar(i);
+		putchar('\n');
+	}
+	return (0);
+
+}
