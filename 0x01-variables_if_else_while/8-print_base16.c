86a319dc270df42c330632156ee723133ce87fba
ikrambl <ikrambelcadi@gmail.com>
2023-04-22 14:31:04 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Cprogram prints all the numbers of base 16
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48; i <= 57; i++)
+{
+	putchar(i);
+}
+for (j = 97; j <= 102; j++)
+{
+	putchar(j);
+}
+putchar('\n');
+return (0);
+	}
