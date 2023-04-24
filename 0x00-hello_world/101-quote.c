ad572f18be1740e25ee218d8d414e28f9e52f747
ikrambl <ikrambelcadi@gmail.com>
2023-04-24 08:21:15 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - a C program that prints a string
+ * Return: 1 (success)
+ */
+int main(void)
+{
+	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, message, sizeof(message) - 1);
+			return (1);
+			}
