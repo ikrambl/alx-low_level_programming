ac75747ee266be3f9f73d36c021b57c62fde1a89
ikrambl <ikrambelcadi@gmail.com>
2023-04-25 15:37:36 -0700

    ./1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_alphabet - Print alphabet
+ */
+void print_alphabet(void)
+{
+    char lett;
+for (lett = 'a'; lett <= 'z'; lett++)
+
+_putchar(lett);
+_putchar('\n');
+}
