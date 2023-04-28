ad4bd9dd28b75a5148c1249d44004964712ee5c9
ikrambl <ikrambelcadi@gmail.com>
2023-04-28 16:59:16 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - prints fizzbuzz
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int count;
+
+for (count = 1; count <= 100; count++)
+{
+if (count % 3 == 0 && !(count % 5 == 0))
+{
+printf("Fizz ");
+}
+else if (!(count % 3 == 0) && count % 5 == 0)
+{
+printf("Buzz ");
+}
+else if (!(count % 3 == 0) && !(count % 5 == 0))
+{
+printf("%d ", count);
+}
+else if (count % 3 == 0 && count % 5 == 0)
+{
+printf("FizzBuzz ");
+}
+else if (count == 100)
+{
+printf("%d", count);
+}
+}
+printf("\n");
+return (0);
+}
