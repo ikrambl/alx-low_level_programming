e63fc1894bc9c94876f928631a150ed6c789c970
ikrambl <ikrambelcadi@gmail.com>
2023-04-28 14:55:59 -0700

    ./5-more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * more_numbers -  prints 10 times the numbers,
+ *
+ * Return void
+ */
+void more_numbers(void)
+{
+int times;
+int i;
+
+for (times = 0; times <= 10; times++)
+{
+for (i = 0; i <= 14; i++)
+{
+if (i > 9)
+{
+_putchar(1 + 48);
+}
+_putchar(i % 10 + 48);
+}
+_putchar('\n');
+}
+}
