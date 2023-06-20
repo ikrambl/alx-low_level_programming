e1cec98bfe984feed299274a61b06f2c7ea00a46
ikrambl <ikrambelcadi@gmail.com>
2023-06-20 13:02:25 -0700

    up

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog structure
+ * @name:dog's name
+ * @age: dog's age
+ * @owner: dog owner's name
+ *
+ * Description: this is a fog struct
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
